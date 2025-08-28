93a24552e62ca9aebc1241dbb428a0435f4d0e62
Tzvi Zait <z0548430441@gmail.com>
2025-08-28 10:22:42 +0300

    I created a .env file and a skip for message

--- Retriever/dal.py
+++ Retriever/dal.py
@@ -1,17 +1,21 @@
-import time
-from datetime import date, datetime
+import os
 
-import schedule
-import pymongo
+from dotenv import load_dotenv
 
 from pymongo import MongoClient
+load_dotenv()
+
+USER = os.getenv("USER")
+PASS = os.getenv("PASS")
+
 
 class DAL:
 
     def __init__(self):
-        self.CONNECTION_STRING = "mongodb+srv://IRGC_NEW:iran135@cluster0.6ycjkak.mongodb.net/"
 
-    def get_database(self):
+        self.CONNECTION_STRING = f"mongodb+srv://{USER}:{PASS}@cluster0.6ycjkak.mongodb.net/"
+
+    def get_database(self,skip = 0):
 
         client = MongoClient(self.CONNECTION_STRING)
 
@@ -19,7 +23,7 @@ class DAL:
 
         collection = client['tweets']
 
-        query = collection.find({},{"_id":0}).sort('CreateDate',1).skip(0).limit(100)
+        query = collection.find({},{"_id":0}).sort('CreateDate',1).skip(skip).limit(100)
 
         return query
 
