ba5fe829a79a22e1d4f6a16e26b262c8e35fcd81
Avram <ad@dermer.co.il>
2025-08-28 10:41:36 +0300

    fixed two subs lisent togeser

--- Enricher/Subscriber.py
+++ Enricher/Subscriber.py
@@ -7,22 +7,25 @@ from kafka import KafkaConsumer
 
 class EnricherSubscriber:
 
-    def __init__(self,kafka_address,topic_name,target_topic_name,):
+    def __init__(self,kafka_address,topics_names_1,topics_names_2,target_topic_name_1,target_topic_name_2):
         self.kafka_pusher = Push_kafka(kafka_address)
         self.kafka_pusher.connect()
 
         self.consumer = KafkaConsumer(
-            topic_name,
+            topics_names_1,topics_names_2,
             bootstrap_servers=kafka_address,
             auto_offset_reset='earliest',  
             group_id='my-group'
         )
         i = 0
         for message in self.consumer:
-            message = json.loads(message.value.decode('utf-8'))
+            message_val = json.loads(message.value.decode('utf-8'))
             addFilds = ProcesseData.processing(message)
             for fild in addFilds.keys():
-                message[fild] = addFilds[fild]
-            self.kafka_pusher.send_by_topic_name(target_topic_name,message)
-            print(F"messeage {i} sended")
+                message_val[fild] = addFilds[fild]
+            if message.topic == topics_names_1:
+                self.kafka_pusher.send_by_topic_name(target_topic_name_1,message_val)
+            else:
+                self.kafka_pusher.send_by_topic_name(target_topic_name_2,message_val)
+            print(F"messeage {i} sended {message.topic}")
             i += 1
--- Preprocessor/main.py
+++ Preprocessor/main.py
@@ -3,7 +3,5 @@ from Preprocessor.Subscriber import CleanerSubscriber
 
 
 if __name__ == "__main__":
-    antisemitic = CleanerSubscriber("localhost:9092","raw_tweets_antisemitic",'preprocessed_tweets_antisemitic')
-    # antisemitic = CleanerSubscriber("localhost:9092","interesting",'preprocessed_tweets_antisemitic')
-    not_antisemitic = CleanerSubscriber("localhost:9092","raw_tweets_not_antisemitic",'preprocessed_tweets_not_antisemitic')
-    # not_antisemitic = CleanerSubscriber("localhost:9092","not_interesting",'preprocessed_tweets_not_antisemitic')
+    antisemitic = CleanerSubscriber("localhost:9092","raw_tweets_antisemitic",'raw_tweets_not_antisemitic','preprocessed_tweets_antisemitic','preprocessed_tweets_not_antisemitic')
+    # not_antisemitic = CleanerSubscriber("localhost:9092","raw_tweets_not_antisemitic",'preprocessed_tweets_not_antisemitic')
